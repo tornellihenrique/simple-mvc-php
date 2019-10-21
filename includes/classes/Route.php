421430139cbe067efd3b95405ef2da400725a016
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2019-10-21 19:32:47 -0300

    ajustando esse cu

--- includes/classes/Route.php
+++ includes/classes/Route.php
@@ -1,18 +1,18 @@
 <?php
 
-    class Route {
+class Route {
 
-        public static $validRoutes = array();
+    public static $validRoutes = array();
 
-        public static function set($route, $function) {
-            self::$validRoutes[] = $route;
+    public static function set($route, $function) {
+        self::$validRoutes[] = $route;
 
-            // print_r(self::$validRoutes);
+        // print_r(self::$validRoutes);
 
-            if ($_GET['url'] == $route) {
-                $function->__invoke();
-            }
+        if ($_GET['url'] == $route) {
+            $function->__invoke();
         }
     }
+}
 
 ?>
